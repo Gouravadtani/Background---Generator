3e8e05c109a9ab5b6297c7d5d0ea397b9250fc26
Gouravadtani <65698138+Gouravadtani@users.noreply.github.com>
2020-06-29 10:02:26 +0530

    adding starting project

---
+++ script.js
@@ -0,0 +1,28 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+  body.style.background =
+   "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    +color2.value
+    + ")";
+
+     css.textContent = body.style.background + ";";
+}
+//console.log(body);//
+
+/*console.log(css);
+console.log(color1);
+console.log(color2);*/
+
+color1.addEventListener("input", setGradient);
+
+
+  //console.log(color1.value);//
+
+
+color2.addEventListener("input", setGradient);
